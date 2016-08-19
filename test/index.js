53faf8535726bc5cf622e8e5746cfd9d4ceb8e89
Pudding <aideen@aideen.pw>
2016-08-19 14:28:49 -0700

    Remove non-scalable test

--- test/index.js
+++ test/index.js
@@ -1,13 +1,6 @@
 const test = require('tape')
 const mock = require('mock-require')
 
-test('petpet is called when run', t => {
-  t.plan(1)
-  mock('../lib', () => t.pass('mock run'))
-  require('../bin/petpet')
-  mock.stopAll()
-})
-
 test('petpet is a function', t => {
   t.is(typeof require('../lib'), 'function', 'typeof check')
   t.end()
